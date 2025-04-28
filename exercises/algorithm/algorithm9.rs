ef71e64df33d3c6d797c0a0d0ee574c9ec9c5be1
build <build@deb.deb>
2025-04-28 18:06:47 +0800

    update

--- exercises/algorithm/algorithm9.rs
+++ exercises/algorithm/algorithm9.rs
@@ -2,10 +2,10 @@
 	heap
 	This question requires you to implement a binary heap function
 */
-// I AM NOT DONE
 
 use std::cmp::Ord;
 use std::default::Default;
+use std::process::id;
 
 pub struct Heap<T>
 where
@@ -37,8 +37,21 @@ where
     }
 
     pub fn add(&mut self, value: T) {
-        //TODO
-    }
+        self.items.push(value);
+        self.count += 1;
+
+        let mut idx = self.count;
+        // 上浮操作
+        while idx > 1 {
+            let parent_idx = idx / 2;
+            if (self.comparator)(&self.items[idx], &self.items[parent_idx]) {
+                self.items.swap(idx, parent_idx);
+                idx = parent_idx;
+            } else {
+                break;
+            }
+        }
+    }   
 
     fn parent_idx(&self, idx: usize) -> usize {
         idx / 2
@@ -57,8 +70,18 @@ where
     }
 
     fn smallest_child_idx(&self, idx: usize) -> usize {
-        //TODO
-		0
+        let left = self.left_child_idx(idx);
+        let right = self.right_child_idx(idx);
+
+        if right > self.count {
+            left
+        } else {
+            if (self.comparator)(&self.items[left], &self.items[right]) {
+                left 
+            } else {
+                right
+            }
+        }
     }
 }
 
@@ -84,8 +107,30 @@ where
     type Item = T;
 
     fn next(&mut self) -> Option<T> {
-        //TODO
-		None
+        if self.count == 0 {
+            return None;
+        }
+        // get stack top element idx 1
+        let result = self.items.swap_remove(1);
+        self.count -= 1;
+
+        if self.count > 0 {
+            // 下沉操作
+            let mut idx = 1;
+            while self.children_present(idx) {
+                let child = self.smallest_child_idx(idx);
+
+                if !(self.comparator)(&self.items[idx], &self.items[child]) {
+                    self.items.swap(idx, child);
+                    idx = child;
+                } else {
+                    break;
+                }
+                
+            }
+        }
+
+        Some(result)
     }
 }
 
